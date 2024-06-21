6312f4e781515f446a8a241eacb9952db3c4ad98
Jimena Zitaglia <jimezi@hotmail.com>
2024-06-21 12:11:15 -0300

    js inicio modificado

--- js/inicio.js
+++ js/inicio.js
@@ -1,6 +1,14 @@
 
-let mensaje = prompt("¿Estás en una situación urgente y NO PODES HABLAR? Dejanos tu contacto")
+let mensaje = prompt("¿Estás en una situación urgente y NO PODES HABLAR? Dejanos tu nombre y contacto de alguien confiable para vos!")
+
+if (mensaje == "") {
+    alert("Esperamos que nuestra Web te ayude en tu búsqueda de información!");
+
+}
+
+else {
+    alert("Gracias, pronto nos pondremos en contacto con la información que proporcionaste: "+mensaje+"!")
+}
 
 console.log(mensaje)
 
-alert ("Gracias, pronto nos pondremos en contacto!")
