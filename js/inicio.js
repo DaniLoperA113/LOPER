0e7fd4f7d322d68acb84aae26804d05dc0ecc85b
LoreleyJoray <103383015+LoreleyJoray@users.noreply.github.com>
2024-07-30 01:36:54 -0300

    Update inicio.js

--- js/inicio.js
+++ js/inicio.js
@@ -1,4 +1,4 @@
-
+function subscripcion(){
 let mensaje = prompt("¿Estás en una situación urgente y NO PODES HABLAR? Dejanos tu nombre y contacto de alguien confiable para vos!")
 
 if (mensaje == "" || mensaje == null) {
@@ -6,5 +6,5 @@ if (mensaje == "" || mensaje == null) {
 } else {
     alert("Gracias, pronto nos pondremos en contacto con la información que proporcionaste: "+mensaje+"!")
 }
-
+}
 // console.log(mensaje)
