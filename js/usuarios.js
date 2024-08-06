bea5563063653be1d835af9b61be9d372a6cc068
DaniLoperA113 <daniloper.a113@gmail.com>
2024-08-06 10:45:21 -0300

    Formulario de registro con storage

---
+++ js/usuarios.js
@@ -0,0 +1,26 @@
+/*
+let dbUsuarios = 
+[   
+    {
+        "nombre" : "nombre",
+        "email" : "email",
+        "password" : "password"
+    },
+    {
+        "nombre" : "daniela",
+        "email" : "daniela@gmail.com",
+        "password" : "1234"
+    },
+    {
+        "nombre" : "jimena",
+        "email" : "jimena@gmail.com",
+        "password" : "5678"
+    },
+    {
+        "nombre" : "lorelei",
+        "email" : "lorelei@gmail.com",
+        "password" : "9123"
+    }
+]
+module.exports = dbUsuarios
+*/
